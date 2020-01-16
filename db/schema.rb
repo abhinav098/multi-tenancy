557cc15542ef466b432069586e651b7ad60247f3
Abhinav Garg <abhinav.g@kiprosh.com>
2020-01-16 11:41:59 +0530

    multi tenancy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_055618) do
+ActiveRecord::Schema.define(version: 2020_01_16_061101) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
